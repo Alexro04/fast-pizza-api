35e377fdfeb63e16a6ce7ea793e37029e534fa44
Alexro04 <ajakatomi@gmail.com>
2025-03-28 15:14:53 +0100

    user model for authentication

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
